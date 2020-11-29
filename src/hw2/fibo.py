e1ceb7648fe7c5eaec2a9dcb3ecdce9d675e9f21
matvej.tarasov <matvej.tarasov@ltlatn.com>
2020-11-29 20:59:55 +0300

    HW.2 ex.3 update matrix

--- src/hw2/fibo.py
+++ src/hw2/fibo.py
@@ -36,23 +36,64 @@ def matrix_fib(n):
     return result[0][0]
 
 
-def matrix_multiplication(first_matrix, second_matrix):
-    result_matrix = []
-    for (x, row) in enumerate(first_matrix):
-        result_matrix.append([0] * len(row))
-        for (y, col) in enumerate(second_matrix):
-            result_matrix[x][y] = 0
-            for i in range(len(row)):
-                result_matrix[x][y] += row[i] * col[i]
-    return result_matrix
+def matrix_multiplication(M1, M2):
+    a11 = M1[0][0] * M2[0][0] + M1[0][1] * M2[1][0]
+    a12 = M1[0][0] * M2[0][1] + M1[0][1] * M2[1][1]
+    a21 = M1[1][0] * M2[0][0] + M1[1][1] * M2[1][0]
+    a22 = M1[1][0] * M2[0][1] + M1[1][1] * M2[1][1]
+    r = [[a11, a12], [a21, a22]]
+    return r
+
+
+class MatrixFibonacci:
+    Q = [[1, 1],
+         [1, 0]]
+
+    def __init__(self):
+        self.__memo = {}
+
+    def __get_matrix_power(self, M, p):
+        """Возведение матрицы в степень (ожидается p равная степени двойки)."""
+
+        if p == 1:
+            return M
+        if p in self.__memo:
+            return self.__memo[p]
+        K = self.__get_matrix_power(M, int(p / 2))
+        R = matrix_multiplication(K, K)
+        self.__memo[p] = R
+        return R
+
+    def get_number(self, n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        # Разложение переданной степени на степени, равные степени двойки,
+        # т.е. 62 = 2^5 + 2^4 + 2^3 + 2^2 + 2^0 = 32 + 16 + 8 + 4 + 1.
+        powers = [int(pow(2, b))
+                  for (b, d) in enumerate(reversed(bin(n - 1)[2:])) if d == '1']
+
+        matrices = [self.__get_matrix_power(MatrixFibonacci.Q, p)
+                    for p in powers]
+        while len(matrices) > 1:
+            M1 = matrices.pop()
+            M2 = matrices.pop()
+            R = matrix_multiplication(M1, M2)
+            matrices.append(R)
+        return matrices[0][0][0]
 
 
 if __name__ == '__main__':
+    # решение с мемоизацией
+    # работает быстрее на больших значениях
+    m = MatrixFibonacci()
     t = test.ComparableTest(
         {
             'Рекурсия': rec_fib,
             'Итерация': iterative_fib,
             'Золотое сечение': golden_section,
-            'Матричное умножение': matrix_fib
+            'Матричное умножение': matrix_fib,
+            'Матричное умножение memo': m.get_number
         }, 'problems/2/4.Fibo', int, int)
     t.start()
